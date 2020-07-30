ba9b90630ab7f4f5d121486584e605689e783924
melani2003a <53543780+melani2003a@users.noreply.github.com>
2020-07-30 07:32:51 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <table>
+<thead>
+<tr>
+<th>NOMBRES</th>
+<th>APELLIDOS</th>
+<th>CORREO</th>
+<th>CELULAR</th>
+  </tr>
+  </thead>
+<tbody>
+  <tr>
+<td>Melani</td>
+<td>Alvarez Gonzalez</td>
+<td>melodialvarez18@gmail.com</td>
+<td>3005940737</td>
+</tr>
+<tr>
+<td>Mariana</td>
+<td>Orozco</td>
+<td>marianamartinezoro@gmail.com</td>
+<td>3105131393</td>
+</tr>
+<tr>
+<td>Veronica</td>
+<td>Restrepo Caicedo</td>
+<td>veronicarestrepocaicedo@gmail.com</td>
+<td>3022624672</td>
+</tr>
+</tbody>
+
+
+        </table>
+
+    </div>
+  );
+}
